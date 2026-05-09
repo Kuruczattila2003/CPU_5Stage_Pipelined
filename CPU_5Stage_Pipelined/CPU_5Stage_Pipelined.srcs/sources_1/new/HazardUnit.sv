220d1f5ff63a58cb45f3e3f94eea9c44c608252e
Kurucz Attila <kuruczattila2b@gmail.com>
2026-05-09 19:08:35 +0200

    Hazard Unit forwarding implemented

--- CPU_5Stage_Pipelined/CPU_5Stage_Pipelined.srcs/sources_1/new/HazardUnit.sv
+++ CPU_5Stage_Pipelined/CPU_5Stage_Pipelined.srcs/sources_1/new/HazardUnit.sv
@@ -1,3 +1,40 @@
-module HazardUnit();
+module HazardUnit(
+    input logic [4:0] RSrc1D, RSrc2D,
+    input logic [4:0] RSrc1E, RSrc2E,
+    input logic [4:0] RDE, RDM, RDW,
+    input logic RegisterFileWEM, RegisterFileWEW,
+    
+    output logic [1:0] ForwardAE, ForwardBE
+    
+);
 
+    always_comb begin
+    //Forward ALUSrcA
+    if((RSrc1E == RDM) && (RSrc1E != 5'b0) && RegisterFileWEM) begin
+        ForwardAE = 2'b10;
+    end
+    else if((RSrc1E == RDW) && (RSrc1E != 5'b0) && RegisterFileWEW) begin
+        ForwardAE = 2'b01;
+    end
+    else begin
+        ForwardAE = 2'b00;
+    end
+    
+    //Forward ALUSrcB
+    if((RSrc2E == RDM) && (RSrc2E != 5'b0) && RegisterFileWEM) begin
+        ForwardBE = 2'b10;
+    end
+    else if((RSrc2E == RDW) && (RSrc2E != 5'b0) && RegisterFileWEW) begin
+        ForwardBE = 2'b01;
+    end
+    else begin
+        ForwardBE = 2'b00;
+    end
+    
+    //Stall for Load
+    
+    //Flush for jump and branch -> branch missprediction
+    
+    end
+    
 endmodule
